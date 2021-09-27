51609e7e6e6886583d954af4e14ca66cacd222f5
Prith Balaji <prithvibalaji16@gmail.com>
2021-09-27 16:39:48 -0700

    Add project files.

---
+++ CS155Lab5SpeedLimits/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS155Lab5SpeedLimits
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Speed Limits";
+            lblStatus.Content = "";
+            txtSpeedLimit.Text = "65";
+
+            txtMySpeed.Focus();
+
+        }
+
+        private void txtSpeedLimit_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtSpeedLimit.SelectAll();
+        }
+
+        private void txtMySpeed_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtMySpeed.SelectAll();
+        }
+
+        private void cmdCheckSpeedLimit_OnClick(object sender, RoutedEventArgs e)
+        {
+            double dSpeedLimit, dMySpeed;
+            bool bResult;
+
+            bResult = Double.TryParse(txtSpeedLimit.Text, out dSpeedLimit);
+            if (!bResult)
+            {
+                MessageBox.Show("Please enter a valid Speed Limit!");
+                return;
+            }
+
+            bResult = Double.TryParse(txtMySpeed.Text, out dMySpeed);
+            if (!bResult)
+            {
+                MessageBox.Show("Please enter a valid Speed for you!");
+                return;
+            }
+
+            if (dMySpeed <= dSpeedLimit)
+            {
+                canvasMain.Background = Brushes.Green;
+                lblStatus.Content = "Status: Legal Speed";
+            }
+            else if ((dMySpeed - dSpeedLimit) < 5.0)
+            {
+                canvasMain.Background = Brushes.Yellow;
+                lblStatus.Content = "Status: Warning - Speeding, no Penalty";
+            }
+            else
+            {
+                canvasMain.Background = Brushes.Red;
+                lblStatus.Content = "Status: Warning - Speeding, Penalty";
+
+            }
+
+
+        }
+    }
+}
